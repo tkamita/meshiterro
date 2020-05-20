1046d5f0bf0459314e608b6f666d8d0a3a04aa31
vagrant <vagrant@localhost.localdomain>
2020-05-20 11:56:28 +0000

    [update]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'post_images#index'
   resources :post_images, only: [:new, :create, :index, :show] do
- 	resources :post_comments, only: [:create, :destroy]
- end
+		resource :favorites, only: [:create, :destroy]
+		resources :post_comments, only: [:create, :destroy]
+	end
 end
-
 
 
