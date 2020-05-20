c077d5dd410c4402c15dc0e25f7a6f101b216046
vagrant <vagrant@localhost.localdomain>
2020-05-20 12:53:04 +0000

    [update]バリデーション

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -5,8 +5,11 @@ class PostImagesController < ApplicationController
 	def create
 		@post_image = PostImage.new(post_image_params)
 		@post_image.user_id = current_user.id
-		@post_image.save
-		redirect_to post_images_path
+		if @post_image.save
+			redirect_to post_images_path
+		else
+			render :new
+		end
 	end
 	def index
 		@post_images = PostImage.page(params[:page]).reverse_order
@@ -21,4 +24,7 @@ class PostImagesController < ApplicationController
 			params.require(:post_image).permit(:shop_name, :image, :caption)
 		end
 end
+
+
+
 
